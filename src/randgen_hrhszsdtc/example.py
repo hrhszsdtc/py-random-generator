7d75cee60ab90dc2028a6081f25267f172011c73
hrh233 <77053088+hrh233@users.noreply.github.com>
2023-09-23 11:59:52 +0800

    Update example.py

--- src/randgen_hrhszsdtc/example.py
+++ src/randgen_hrhszsdtc/example.py
@@ -1,4 +1,8 @@
 from __init__ import *
 
-print(gen_raw_data(),type(gen_raw_date()))
-pass
+print(gen_rawdata(),type(gen_rawdata()))
+print(gen_random_int(1,32),type(gen_random_int(1,32)))
+print(gen_random_bytes(None),type(gen_random_bytes(None)))
+print(gen_random_urlsafe_token(None),type(gen_random_urlsafe_token(None)))
+print(gen_random_single_digit(),type(gen_random_single_digit()))
+print(gen_random_true_or_false(),type(gen_random_true_or_false()))
